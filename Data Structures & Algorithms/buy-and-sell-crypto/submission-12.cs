6606c0c1eeeb7ea286e6a281d217e0f2de412a63
proprzemek <pszmajdzi123@gmail.com>
2026-05-07 17:45:00 +0200

    Add: buy-and-sell-crypto - submission-12

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-12.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach (int price in prices) {
+            minPrice = Math.Min(minPrice, price);
+            
+            maxProfit = Math.Max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
