cedb4810ab4533e0bf670955403330d37df174e3
proprzemek <pszmajdzi123@gmail.com>
2026-05-11 12:10:09 +0200

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int prev2 = 1;
+        int prev1 = 2;
+        int current = 0;
+
+        for (int i = 3; i <= n; i++) {
+            current = prev1 + prev2;
+            
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+}
