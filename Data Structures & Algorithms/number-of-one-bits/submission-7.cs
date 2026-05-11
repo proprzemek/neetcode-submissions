61f38a532e4c2283a57e14b94a5cda9534a7950e
proprzemek <pszmajdzi123@gmail.com>
2026-05-11 13:08:43 +0200

    Add: number-of-one-bits - submission-7

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-7.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int HammingWeight(uint n) {
+        int count = 0;
+        
+        while (n != 0) {
+
+            n = n & (n - 1); 
+            
+            count++; 
+        }
+        
+        return count;
+    }
+}
